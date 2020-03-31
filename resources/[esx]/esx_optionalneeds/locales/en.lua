e89091b5b5966c4e042b68d372fdcf07b8747e19
root <root@ks309388.kimsufi.com>
2020-03-31 17:57:34 +0000

    Resources

---
+++ resources/[esx]/esx_optionalneeds/locales/en.lua
@@ -0,0 +1,20 @@
+Locales['en'] = {
+	
+	['used_beer'] = 'you used 1x ~y~Beer~s~',
+	['used_wine'] = 'you used 1x ~y~Wine~s~',
+	['used_vodka'] = 'you used 1x ~y~Vodka~s~',
+	['used_tequila'] = 'you used 1x ~y~Tequila~s~',
+	['used_whisky'] = 'you used 1x ~y~Whisky~s~',
+	['used_jager'] = 'you used 1x ~y~Jager~s~',
+	['used_rhum'] = 'you used 1x ~y~Rhum~s~',
+	['used_martini'] = 'you used 1x ~y~Martini~s~',
+	['used_whiskycoca'] = 'you used 1x ~y~WhiskyCoca~s~',
+	['used_vodkaenergy'] = 'you used 1x ~y~VodkaEnergy~s~',
+	['used_vodkafruit'] = 'you used 1x ~y~VodkaFruit~s~',
+	['used_rhumfruit'] = 'you used 1x ~y~RhumFruit~s~',
+	['used_teqpah'] = 'you used 1x ~y~TeqPah~s~',
+	['used_rhumcoca'] = 'you used 1x ~y~RhumCoca~s~',
+	['used_mojito'] = 'you used 1x ~y~Mojito~s~',
+	['used_jagercerbere'] = 'you used 1x ~y~JagerCerbere~s~',
+
+}
---
+++ resources/[esx]/esx_teleports/config.lua
@@ -0,0 +1,41 @@
+Config                            = {}
+
+Config.Teleporters = {
+	['Processing_Center'] = {
+		['Job'] = 'unemployed',
+		['Job'] = 'SASP',
+		['Enter'] = { 
+			['x'] = 446.35, 
+			['y'] = -986.56, 
+			['z'] = 26.67,
+			['Information'] = 'Press [E] to Enter Processing Center',
+		},
+		['Exit'] = {
+			['x'] = 403.5, 
+			['y'] = -984.5, 
+			['z'] = -99, 
+			['Information'] = 'Press [E] to Exit Processing Center' 
+		}
+	},
+
+	['Cardealer'] = {
+		['Job'] = 'none',
+		['Enter'] = { 
+			['x'] = 0, 
+			['y'] = 0, 
+			['z'] = 0,
+			['Information'] = '[E] Go Up' 
+		},
+
+		['Exit'] = {
+			['x'] = 0, 
+			['y'] = 0, 
+			['z'] = 0, 
+			['Information'] = '[E] Go Down' 
+		}
+	}
+
+	--Next here
+}
+
+
